4db404dba606c080e642697f22382d162ce9f83b
hiwi-dci-student <hiwot.abdi@dci.education>
2023-06-15 10:23:02 +0200

    Embedding itemSchema as a sub-document array

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,16 +1,34 @@
+// import { Schema, model } from "mongoose";
+
+// const cartSchema = new Schema({
+//   customerId: { type: Schema.Types.ObjectId, ref: "customer" },
+//   items: [
+//     {
+//       productId: { type: Schema.Types.ObjectId, ref: "product" },
+//       quantity: { type: Number, required: true },
+//       price: { type: Number, required: true },
+//       vat: { type: Number, required: true },
+//       totalPrice: { type: Number, required: true },
+//     },
+//   ],
+// });
+
+// const Cart = model("cart", cartSchema);
+// export default Cart;
+
 import { Schema, model } from "mongoose";
+
+const itemSchema = new Schema({
+  productId: { type: Schema.Types.ObjectId, ref: "product" },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  vat: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
+});
 
 const cartSchema = new Schema({
   customerId: { type: Schema.Types.ObjectId, ref: "customer" },
-  items: [
-    {
-      productId: { type: Schema.Types.ObjectId, ref: "product" },
-      quantity: { type: Number, required: true },
-      price: { type: Number, required: true },
-      vat: { type: Number, required: true },
-      totalPrice: { type: Number, required: true },
-    },
-  ],
+  items: [itemSchema], // Embedding itemSchema as a sub-document array
 });
 
 const Cart = model("cart", cartSchema);
